27d22dd425a1484842539181c83c97b2d4249ae8
Jose Manoel <joseneto190306@gmail.com>
2026-03-02 15:14:41 -0300

    Primeiro programa em Golang.

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Olá Mundo! Estou estudando a linguagem GO.")
+	fmt.Println("Estou me livrando da maldição e começando a aprender uma nova linguagem de programação.")
+	fmt.Println("Testando o comando 'go run hello.go' para executar meu código.")
+}
